a11ade22c1df1a67dee033561bb96c2b4d1690b0
surendrasai1827 <saichannel2001@gmail.com>
2026-01-13 17:53:15 +0530

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,26 +1,24 @@
 from telegram.ext import Application, MessageHandler, filters
 
-BOT1_TOKEN = "8434050747:AAGyny5RBwXZp6KM53vQO2CTzzBO0LvhU4Y"
+BOT1_TOKEN = "8434050747:AAG7vGcZVanylxrJ2bR7lyxJ6GLqgfI4scM"
+GROUP_A = -1003613099017
+GROUP_B = -1003624477633
 
-GROUP_A = -1003613099017   # Group A
-GROUP_B = -1003624477633   # Group B
+TAG = "[BOT1]"
 
 async def forward(update, context):
     msg = update.message
     if not msg:
         return
 
-    # Ignore messages sent by THIS bot itself
-    if msg.from_user and msg.from_user.username == context.bot.username:
+    text = msg.text or msg.caption or ""
+    if TAG in text:
         return
 
-    text = msg.text or msg.caption or ""
-
     if msg.chat_id == GROUP_A:
-        await msg.copy(chat_id=GROUP_B, caption=text)
-
+        await msg.copy(chat_id=GROUP_B, caption=text + " " + TAG)
     elif msg.chat_id == GROUP_B:
-        await msg.copy(chat_id=GROUP_A, caption=text)
+        await msg.copy(chat_id=GROUP_A, caption=text + " " + TAG)
 
 app = Application.builder().token(BOT1_TOKEN).build()
 app.add_handler(MessageHandler(filters.ALL, forward))
